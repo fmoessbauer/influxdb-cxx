33e06c9f8880e6e28d8b38f590a1f56435e6ffeb
Felix Moessbauer <felix.moessbauer@gmail.com>
2020-10-26 21:06:09 +0100

    update conan package test to new interface

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,8 +1,15 @@
 #include <InfluxDBFactory.h>
+#include <Point.h>
 
 int main()
 {
+    // TODO: in this environment, we do not have a running influxdb
+    // hence, simulate with a batch and clear it before termination.
+    // By that, no http request should be made (this unforunately 
+    // relies on internal implementation details)
     auto influxdb = influxdb::InfluxDBFactory::Get("http://localhost:8086/?db=test");
-    influxdb->addGlobalTag("test", "value");
+    influxdb->batchOf(2);
+    influxdb->write(influxdb::Point{"test"});
+    influxdb->clearBatch();
     return 0;
 }
